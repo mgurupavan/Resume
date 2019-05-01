be28943564d38fa952e994618bb582c48a9c7413
gurupavan <mgurupavan@gmail.com>
2019-05-01 15:53:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Link to="/personalinfo">Personal details </Link> |
+        <Link to="/educationalinfo"> Educational Info </Link> |
+        <Link to="/projectsdone"> Projects Done </Link> |
+        <Link to="/technicalSkills"> Technical Skills</Link>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
